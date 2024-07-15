3746aa662a4da8982a3c06b5efceddcec70b7876
Ryan Seiyu <ryan_seiyu@hotmail.com>
2024-07-15 11:53:39 -0400

    Initial commit with Spline integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import Spline from '@splinetool/react-spline'; // Import Spline
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Spline scene="https://prod.spline.design/78Ldjfp770bmw7rD/scene.splinecode" /> {/* Add Spline component */}
       </header>
     </div>
   );
